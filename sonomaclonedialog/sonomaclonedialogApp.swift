18ced21117d147f7d1b3a581a7761a8ec3be7da6
Fazlı Altun <32793348+fazlialtunn@users.noreply.github.com>
2024-05-26 23:56:44 +0300

    Initial Commit

---
+++ sonomaclonedialog/sonomaclonedialogApp.swift
@@ -0,0 +1,17 @@
+//
+//  sonomaclonedialogApp.swift
+//  sonomaclonedialog
+//
+//  Created by Fazli Altun on 26.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct sonomaclonedialogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
